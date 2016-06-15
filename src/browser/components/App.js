4acb8ad1ceb41a192adfd4c5418ac0df13e1af86
Siarhei Melnik <siarhei.m.a@gmail.com>
2016-06-16 00:19:15 +0300

    styles

--- src/browser/components/App.js
+++ src/browser/components/App.js
@@ -1,16 +1,25 @@
 import './App.scss';
 import Rx from 'rx';
-import { div, h1 } from '@cycle/dom';
+import { div, h1, h2 } from '@cycle/dom';
 
 function App() {
   const view = () =>
-    div('#app .pure-g', [
-      h1('.title', ['APP'])
+    div('#layout .pure-g', [
+      div('.sidebar pure-u-1 pure-u-md-1-4', [
+        div('.header', [
+          h1('.brand-title', 'Breeze'),
+          h2('.brand-tagline', 'FRP application')
+        ])
+      ]),
+      div('.content pure-u-1 pure-u-md-3-4', [
+        div([
+          h1('.content-subhead', 'How to use FRP')
+        ])
+      ])
     ]);
-  };
 
   const view$ = Rx.Observable.just(
-    view(sources)
+    view()
   );
 
   const sinks = {
