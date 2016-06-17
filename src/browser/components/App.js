c5d3404a081f190dec63cf4cd38923aecb67abe1
Siarhei Melnik <siarhei.m.a@gmail.com>
2016-06-17 20:42:26 +0300

    refctor: upd

--- src/browser/components/App.js
+++ src/browser/components/App.js
@@ -1,32 +1,29 @@
 import './App.scss';
-import Rx from 'rx';
-import { div, h1, h2 } from '@cycle/dom';
+import { Observable } from 'rx';
+import { h } from '@cycle/dom';
 
 function App() {
-  const view = () =>
-    div('#layout .pure-g', [
-      div('.sidebar pure-u-1 pure-u-md-1-4', [
-        div('.header', [
-          h1('.brand-title', 'Breeze'),
-          h2('.brand-tagline', 'FRP application')
+  const view = () => Observable.just(
+    h('div#layout .pure-g', [
+      h('div.sidebar pure-u-1 pure-u-md-1-4', [
+        h('div.header', [
+          h('h1.brand-title', 'Breeze'),
+          h('h2.brand-tagline', 'FRP application')
         ])
       ]),
-      div('.content pure-u-1 pure-u-md-3-4', [
-        div([
-          h1('.content-subhead', 'How to use FRP')
+      h('div.content pure-u-1 pure-u-md-3-4', [
+        h('div', [
+          h('h1.content-subhead', 'How to use FRP')
         ])
       ])
-    ]);
-
-  const view$ = Rx.Observable.just(
-    view()
+    ])
   );
 
-  const sinks = {
-    DOM: view$,
-  };
+  const view$ = view();
 
-  return sinks;
+  return {
+    DOM: view$
+  };
 }
 
 export default App;
