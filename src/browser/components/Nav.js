4a0800513a1b594c271761257442ba950bc46ffd
Siarhei Melnik <siarhei.m.a@gmail.com>
2016-06-21 22:42:02 +0300

    menu styles

--- src/browser/components/Nav.js
+++ src/browser/components/Nav.js
@@ -5,15 +5,21 @@ export default function Nav(sources) {
   const nav$ = routerSource
     .route$
     .map(() =>
-      h('nav', [
-        a({
-          href: sources.router.buildUrl('home'),
-          className: routerSource.isActive('home') ? 'active' : ''
-        }, 'Home'),
-        a({
-          href: sources.router.buildUrl('hello', { id: 1 }),
-          className: routerSource.isActive('hello') ? 'active' : ''
-        }, 'Hello')
+      h('nav.nav', [
+        h('ul.nav-list', [
+          h('li.nav-item', [
+            a({
+              href: sources.router.buildUrl('home'),
+              className: routerSource.isActive('home') ? 'active pure-button' : 'pure-button'
+            }, 'Home')
+          ]),
+          h('li.nav-item', [
+            a({
+              href: sources.router.buildUrl('hello', { id: 1 }),
+              className: routerSource.isActive('hello') ? 'active pure-button' : 'pure-button'
+            }, 'Hello')
+          ])
+        ])
       ])
     ).startWith();
 
