554f9a31661d726dbafbd5a21eaf97ac367fb60d
Siarhei Melnik <siarhei.m.a@gmail.com>
2016-06-22 19:37:25 +0300

    fix linter

--- src/browser/pages/Hello.js
+++ src/browser/pages/Hello.js
@@ -4,7 +4,8 @@ import { h } from '@cycle/dom';
 export default function Hello() {
   const view = () =>
     h('div', [
-      h('h1.content-subhead', 'Hello')
+      h('h1.content-subhead', 'Hello'),
+      h('p', 'Text')
     ]);
 
   const vtree$ = $.just(
