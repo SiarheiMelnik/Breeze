63c74b462c58aeb77e31a52e45a8b5105bc146c2
Siarhei Melnik <siarhei.m.a@gmail.com>
2016-06-24 22:34:00 +0300

    exclude tests

--- gulp/cover.js
+++ gulp/cover.js
@@ -4,8 +4,9 @@ import { mochaRun } from './utils/runners';
 
 gulp.task('cover', () => {
   const source = 'src/**/*.js';
+  const exclude = '!src/**/__test__/**/*.js';
 
-  return gulp.src(source)
+  return gulp.src([source, exclude])
     .pipe(istanbul({
       includeUntested: true,
       instrumenter: require('isparta').Instrumenter
